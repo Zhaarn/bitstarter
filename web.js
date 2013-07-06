266b51c15f1ab8446858439f66341b1277f3c41f
Zhaarn Maheswaran <zhaarn@gmail.com>
2013-07-06 19:52:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var file_data = fs.readFileSync(index.html);
+
+var buf = new Buffer(file_data);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
