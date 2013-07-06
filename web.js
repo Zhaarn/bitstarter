c7467dfa11f41524391cb19f2d943067dc341c03
Zhaarn Maheswaran <zhaarn@gmail.com>
2013-07-06 20:04:08 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var file_data = fs.readFileSync(index.html);
+    var file_data = fs.readFileSync("index.html");
     var buf = new Buffer(file_data);
   response.send(buf.toString());
 });
